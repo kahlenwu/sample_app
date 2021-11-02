a026fb74b946fe08fa3728128443136638693f63
kahlwnwu <w2321698@gmail.com>
2021-11-02 11:36:47 +0000

    [Add]rubocop test

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,28 +2,31 @@ class TodolistsController < ApplicationController
   def new
     @list = List.new
   end
+
   def create
     # 1.データを新規登録するためのインスタンス作成
     @list = List.new(list_params)
     # 2.データをデータベースに保存するためのsaveメソッド実行
     if @list.save
-    # 3. トップ画面へリダイレクト
-     redirect_to todolist_path(@list.id)
+      # 3. トップ画面へリダイレクト
+      redirect_to todolist_path(@list.id)
     else
-     render :new
+      render :new
     end
   end
 
   def index
-    @lists =List.all
+    @lists = List.all
   end
 
   def show
     @list = List.find(params[:id])
   end
+
   def edit
     @list = List.find(params[:id])
   end
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
@@ -35,9 +38,11 @@ class TodolistsController < ApplicationController
     list.destroy
     redirect_to todolists_path
   end
-    private
-    # ストロングパラメータ
-    def list_params
-      params.require(:list).permit(:title, :body, :image)
-    end
+
+  private
+
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
 end
